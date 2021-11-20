ea3c1ee594e749b8f6e89da03527ab653512f7a7
wwumiww <wumwum10000@gmail.com>
2021-11-21 08:01:16 +0900

    ホームポジションモディファイア機能削除 (※コンパイルのみ確認)

--- keyboards/helix/rev2/keymaps/mine/keymap.c
+++ keyboards/helix/rev2/keymaps/mine/keymap.c
@@ -24,7 +24,7 @@
   #include "audio.h"
 #endif
 
-#include "keymap_jp.h"
+#include "keymap_jp.h"		// 日本語キーボード用コードの取り込み
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
@@ -34,7 +34,6 @@ enum layer_number {
   _L_DEFAULT = 0,
   _L_EDIT,
   _L_MARK,
-  _L_FUNC,
   _L_MAC,  /* macro */
   _L_ADJUST,
   _L_MAX,  /* レイヤー最大数 */
@@ -53,9 +52,9 @@ int RGB_current_mode;
 /* 独自レイヤコントロール */
 typedef struct {
   uint8_t   retro;    /* 単打時に必ずタッピング動作する(true)/タイムアウトする(false) */
-  uint8_t   ownlayer; /* home layer(true) / qmk layer(false) */
-  uint8_t   layer;    /* レイヤー番号 */
-  uint16_t  keycode;  /* 16bitキーコード */
+  uint8_t   layer;        /* レイヤー番号, 0だとレイヤー動作しない */
+  uint16_t  keycode_hold; /* ホールド時送信するモディファイアのキーコード, 0だと何もしない。 */
+  uint16_t  keycode;      /* タップ時送信する16bitキーコード */
   uint16_t  tapping_term; /* タップ判定時間 */
 } layer_def_t;
 
@@ -72,35 +71,28 @@ enum LayerState_e_t {
   LayerStateHold,     // KeyをHoldしている状態
 };
 
-#define TAPPING_TERM2  30    // メインキーのタッピング
-#define TAPPING_TERM3  30    // メインキーのタッピング
+#define TAPPING_TERM_SHORT 100
+#define TAPPING_TERM_LONG 300
+#define TAPPING_TERM2  10    // メインキーのタッピング
 static const layer_def_t layer_def[] = {
-  { false, false, _L_EDIT,   KC_SPC,  TAPPING_TERM, },  /* LM0 */
-  { false, false, _L_EDIT,   KC_SPC,  TAPPING_TERM, },  /* LM1 */
-  { false, false, _L_MARK,   KC_BSPC, TAPPING_TERM, },  /* LM2 */
-  { false, false, _L_FUNC,   KC_ENT,  TAPPING_TERM, },  /* LM3 */
-  { false, false, _L_MAC,    KC_F23,  TAPPING_TERM, },  /* LM4 */
-  { false, false, _L_ADJUST, KC_TAB,  TAPPING_TERM, },  /* LM5 */
-  { false, false, _L_ADJUST, JP_AT,   TAPPING_TERM, },  /* LM6 */
-  { true,  true,  1,         KC_F,    TAPPING_TERM2, },  /* LM7 */
-  { true,  true,  2,         KC_H,    TAPPING_TERM3, },  /* LM8 */
-//  { true,  _L_EDIT,   KC_G,    TAPPING_TERM2, },  /* LM9 */
-//  { true,  _L_EDIT,   KC_J,    TAPPING_TERM2, },  /* LM10 */
+  { false, _L_EDIT,    KC_NO,   KC_SPC,  TAPPING_TERM_SHORT, },  /* 1:LM0 */
+  { false, _L_EDIT,    KC_NO,   KC_SPC,  TAPPING_TERM_SHORT, },  /* 2:LM1 */
+  { false, _L_MARK,    KC_NO,   KC_BSPC, TAPPING_TERM_SHORT, },  /* 3:LM2 */
+  { false, _L_MARK,    KC_NO,   KC_ENT,  TAPPING_TERM_SHORT, },  /* 4:LM3 */
+  { false, _L_MAC,     KC_NO,   KC_F23,  TAPPING_TERM_SHORT, },  /* 5:LM4 */
+  { false, _L_ADJUST,  KC_NO,   KC_TAB,  TAPPING_TERM_LONG, },  /* 6:LM5 */
+  { false, _L_ADJUST,  KC_NO,   JP_AT,   TAPPING_TERM_LONG, },  /* 7:LM6 */
+  { false, _L_DEFAULT, KC_LCTL, JP_DQUO, TAPPING_TERM_LONG, },  /* 8:LSFT */
+  { false, _L_DEFAULT, KC_RCTL, JP_QUOT, TAPPING_TERM_LONG, },  /* 9:RSFT */
 };
 static layer_ctrl_t layer_ctrl[sizeof(layer_def)/sizeof(layer_def[0])] = {0, };
 static uint8_t  layer_ctrl_wait = 0;  /* 待ち状態キー番号, 0=(なし), 1...=(ワンショットレイヤー番号+1)  */
 static uint16_t layer_ctrl_time = 0;  /* 待ち状態開始時刻 */
 static uint8_t  layer_cnt[_L_MAX] = {0,};     /* レイヤーキーが複数押されているケース対応 */
 
-
-uint8_t gHmodLayer = 0;
-static uint8_t gHmodLastLayer[50] = {0, };
-
 static bool process_user_custom_layer(uint16_t keycode, keyrecord_t *record, uint8_t no);
 static bool process_user_custom_layer_otherkey_down(void);
 static void process_user_custom_layer_time_check(void);
-
-static bool process_hmod_event(uint8_t no, bool pressed);
 
 #if 0
 // コンボキー定義
@@ -131,6 +123,7 @@ combo_t key_combos[COMBO_COUNT] = {
 #endif
 
 
+
 #if MATRIX_ROWS == 8 // HELIX_ROWS == 4
 
 // スワップハンド定義
@@ -148,7 +141,7 @@ const keypos_t hand_swap_config[MATRIX_ROWS][MATRIX_COLS] = {
 // 独自レイヤー定義
 const uint8_t defMineKeyCode[MATRIX_ROWS][MATRIX_COLS] = LAYOUT(  \
    6,  0,  0,  0,  0,  0        ,  0,  0,  0,  0,  0,  7,       \
-  20, 19, 18, 17,  8,  0        ,  0,  9, 21, 22, 23, 24,       \
+   8,  0,  0,  0,  0,  0        ,  0,  0,  0,  0,  0,  9,       \
    0,  0,  0,  0,  0,  0        ,  0,  0,  0,  0,  0,  0,       \
    0,  0,  0,  0,  3,  1,  0,  5,  2,  4,  0,  0 , 0,  0        \
 );
@@ -156,17 +149,13 @@ const uint8_t defMineKeyCode[MATRIX_ROWS][MATRIX_COLS] = LAYOUT(  \
 
 #define K_C_F13 LCTL_T(KC_F13)
 #define K_C_CLN RCTL_T(JP_COLN)
-#define K_S_Z   LSFT_T(KC_Z)
-#define K_S_SLS RSFT_T(JP_SLSH)
 #define K_S_BSL RSFT_T(JP_BSLS)
 #define K_OLWIN OSM(MOD_LGUI)     // LWINワンショット
 #define K_ORWIN OSM(MOD_RGUI)     // RWINワンショット
 #define K_OLALT OSM(MOD_LALT)     // LALTワンショット
 #define K_ORALT OSM(MOD_RALT)     // RALTワンショット
 
-#define K_C_DQUO LCTL_T(JP_DQUO)
-#define K_S_PIPE LSFT_T(JP_PIPE)
-#define K_C_PERC RCTL_T(JP_PERC)
+#define K_S_PSCR LSFT_T(KC_PSCR)
 #define K_S_CIRC RSFT_T(JP_CIRC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -176,7 +165,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //,--------+--------+--------+--------+--------+--------.                 ,--------+--------+--------+--------+--------+--------.
         K_C_F13, KC_A   , KC_S   , KC_D   , KC_F   , KC_G                     , KC_H   , KC_J   , KC_K   , KC_L   , JP_SCLN, K_C_CLN, \
     //,--------+--------+--------+--------+--------+--------.                 ,--------+--------+--------+--------+--------+--------.
-        KC_LSFT, K_S_Z  , KC_X   , KC_C   , KC_V   , KC_B                     , KC_N   , KC_M   , KC_COMM, KC_DOT , K_S_SLS, K_S_BSL, \
+        KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B                     , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, K_S_BSL, \
     //,--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------.
         KC_RCTL, SH_MON , K_OLWIN, K_OLALT,KC_BSPC , KC_SPC , KC_SPC , KC_F23 , KC_SPC , KC_ENT , K_ORALT, K_ORWIN, SH_MON , KC_RCTL  \
     //,--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------.
@@ -184,11 +173,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_L_EDIT] = LAYOUT( \
     //,--------+--------+--------+--------+--------+--------.                 ,--------+--------+--------+--------+--------+--------.
-        JP_QUOT, KC_PSCR, KC_HOME, KC_UP  , KC_END , KC_PGUP                  , JP_EXLM, JP_LPRN, JP_RPRN, JP_AMPR, JP_MINS, JP_EQL , \
+        JP_PIPE, KC_F14 , KC_HOME, KC_UP  , KC_END , KC_PGUP                  , JP_LBRC, JP_LPRN, JP_RPRN, JP_RBRC, JP_MINS, JP_EQL , \
     //,--------+--------+--------+--------+--------+--------.                 ,--------+--------+--------+--------+--------+--------.
-       K_C_DQUO, JP_ZKHK, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN                  , KC_BSPC, KC_ESC , KC_DEL , JP_HENK, JP_MHEN,K_C_PERC, \
+        JP_DQUO, JP_ZKHK, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN                  , KC_BSPC, KC_ESC , KC_DEL , JP_HENK, JP_MHEN, JP_QUOT, \
     //,--------+--------+--------+--------+--------+--------.                 ,--------+--------+--------+--------+--------+--------.
-       K_S_PIPE, JP_LCBR, JP_RCBR, JP_LBRC, JP_RBRC, KC_F14                   , JP_UNDS, KC_ENT , JP_DLR , JP_HASH, JP_TILD,K_S_CIRC, \
+       K_S_PSCR, JP_EXLM, JP_HASH, JP_DLR , JP_PERC, JP_TILD                  , JP_UNDS, KC_ENT , JP_LCBR, JP_RCBR, JP_AMPR,K_S_CIRC, \
     //,--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------.
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
     //,--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------.
@@ -196,26 +185,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_L_MARK] = LAYOUT( \
     //,--------+--------+--------+--------+--------+--------.                 ,--------+--------+--------+--------+--------+--------.
-        _______, JP_EQL , JP_RPRN, JP_LPRN, JP_QUOT, JP_LCBR                  , JP_LBRC, JP_7   , JP_8   , JP_9   , JP_MINS, _______, \
+        KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6                    , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , \
     //,--------+--------+--------+--------+--------+--------.                 ,--------+--------+--------+--------+--------+--------.
-        _______, JP_TILD, JP_AMPR, JP_PERC, JP_DLR , JP_RCBR                  , JP_RBRC, JP_4   , JP_5   , JP_6   , JP_CIRC, _______, \
+        KC_LCTL, JP_1   , JP_2   , JP_3   , JP_4   , JP_5                     , JP_6   , JP_7   , JP_8   , JP_9   , JP_0   , KC_RCTL, \
     //,--------+--------+--------+--------+--------+--------.                 ,--------+--------+--------+--------+--------+--------.
-        _______, JP_PIPE, JP_HASH, JP_DQUO, JP_EXLM, JP_UNDS                  , JP_0   , JP_1   , JP_2   , JP_3   , JP_BSLS, _______, \
-    //,--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------.
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, JP_0   , _______, _______, _______, _______  \
-    //,--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------.
-    ),
-
-    [_L_FUNC] = LAYOUT( \
-    //,--------+--------+--------+--------+--------+--------.                 ,--------+--------+--------+--------+--------+--------.
-        _______, KC_F12 , KC_F11 , KC_F10 , KC_F9  , JP_EISU                  , JP_KANA, KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______, \
-    //,--------+--------+--------+--------+--------+--------.                 ,--------+--------+--------+--------+--------+--------.
-        _______, KC_F8  , KC_F7  , KC_F6  , KC_F5  , KC_ESC                   , KC_PSCR, KC_F5  , KC_F6  , KC_F7  , KC_F8  , _______, \
-    //,--------+--------+--------+--------+--------+--------.                 ,--------+--------+--------+--------+--------+--------.
-        _______, KC_F4  , KC_F3  , KC_F2  , KC_F1  , KC_SLCK                  , KC_PAUS, KC_F1  , KC_F2  , KC_F3  , KC_F4  , _______, \
+        KC_LSFT, _______, _______, _______, _______, _______                  , _______, _______, _______, _______, _______, KC_RSFT, \
     //,--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------.
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
-    //,--------+--------+--------+--------+--------+--------+--------+--------+--------+-------+--------+--------+--------+--------.
+    //,--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------.
     ),
 
     [_L_MAC] = LAYOUT( \
@@ -263,24 +240,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     const uint8_t row = record->event.key.row;
     const uint8_t no = defMineKeyCode[row][col];
     if(no) {
-      bool ret;
-      ret = process_hmod_event(no, record->event.pressed);
-      if(!ret) return false;
-      ret = process_user_custom_layer(keycode, record, no);
-      if(!ret) return false;
+      bool ret = process_user_custom_layer(keycode, record, no);
+      if(!ret) return ret; // 処理継続不要なときは直接リターン
     }
   }
   
-  switch (keycode) {
-  case LM0 ... LM15:
-    return process_user_custom_layer(keycode, record, keycode - LM0);
-    break;
-  
-  default:
-    process_user_custom_layer_otherkey_down();  /* layer制御で他キーが押されたことを通知 */
-    break;
-  }  
-    
   return true;
 }
 
@@ -293,7 +257,6 @@ void matrix_scan_user(void) {
 /*--------------------------------------------------------------------------------*/
 /* タッピング時間切り替え(親指・小指キーを長時間とする                            */
 /*--------------------------------------------------------------------------------*/
-#define TAPPING_TERM_LONG 300
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
   const uint8_t row = record->event.key.row;
   const uint8_t col = record->event.key.col;
@@ -315,6 +278,7 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
 /* 2tap  : tap "key"  */
 /* 2hold : hold "key" */
 static bool process_user_custom_layer(uint16_t keycode, keyrecord_t *record, uint8_t no_in) {
+  bool ret = false;
   const uint8_t no = no_in - 1;
   const layer_def_t*  const def = &layer_def[no];
   layer_ctrl_t* const ctrl = &layer_ctrl[no];
@@ -329,20 +293,20 @@ static bool process_user_custom_layer(uint16_t keycode, keyrecord_t *record, uin
     if ((record->event.time - ctrl->time) > def->tapping_term) {
       /* 初回 Down */
       if(!def->retro) { // Mod優先時はMod操作操作する
-        if(!def->ownlayer) {
-          // qmk layerを変える
+        /* レイヤー遷移 */
+        if(def->layer) {
           layer_on(def->layer);
           layer_cnt[def->layer]++;
         }
-        else {
-          // own layerを変える
-          gHmodLayer = def->layer;
+        /* モディファイア送信 */
+        if(def->keycode_hold) {
+          register_code16(def->keycode_hold);
         }
       }
       
       // Down状態に待ち処理付きで入る
       layer_ctrl_wait = no + 1;
-      layer_ctrl_time = timer_read();
+      layer_ctrl_time = record->event.time;
       ctrl->state = LayerStateDown;
       ctrl->keycode = keycode;
     }
@@ -351,6 +315,7 @@ static bool process_user_custom_layer(uint16_t keycode, keyrecord_t *record, uin
       register_code16(keycode);
       ctrl->keycode = keycode;
       ctrl->state = LayerStateHold;
+      //ret = true;
     }
     // 時刻記録
     ctrl->time = record->event.time;
@@ -361,14 +326,16 @@ static bool process_user_custom_layer(uint16_t keycode, keyrecord_t *record, uin
       /* Up */
       if (!def->retro) {  // Mod優先時はMod操作する
         /* レイヤーキーがすべて離されたらレイヤーをOFFにする */
-        if(!def->ownlayer) {
+        /* レイヤーを戻す */
+        if(def->layer) {
           layer_cnt[def->layer]--;
           if(layer_cnt[def->layer] == 0) {
             layer_off(def->layer);
           }
         }
-        else {
-          gHmodLayer = 0;
+        /* モディファイアを離す */
+        if(def->keycode_hold) {
+          unregister_code16(def->keycode_hold);
         }
       }
       
@@ -380,14 +347,16 @@ static bool process_user_custom_layer(uint16_t keycode, keyrecord_t *record, uin
     }
     else if(ctrl->state == LayerStateModHold) {
       /* レイヤーキーがすべて離されたらレイヤーをOFFにする */
-      if(!def->ownlayer) {
+      if(def->layer) {
+        /* レイヤーを戻す */
         layer_cnt[def->layer]--;
         if(layer_cnt[def->layer] == 0) {
           layer_off(def->layer);
         }
       }
-      else {
-        gHmodLayer = 0;
+      /* モディファイアを離す */
+      if(def->keycode_hold) {
+        unregister_code16(def->keycode_hold);
       }
       // 状態遷移
       ctrl->state = LayerStateInit;
@@ -397,30 +366,28 @@ static bool process_user_custom_layer(uint16_t keycode, keyrecord_t *record, uin
       unregister_code16(ctrl->keycode);
       // 状態遷移
       ctrl->state = LayerStateInit;
+      //ret = true;
     }
     /* Up時刻記録 */
     ctrl->time = record->event.time;
   }
-  return false;
+  return ret;
 }
 
 
 /* 他キーの tapが入ったときに呼び出す(状態変数ビットを1にするだけ) */
 static bool process_user_custom_layer_otherkey_down(void) {
   if(layer_ctrl_wait) {
+    bool ret = false; // レイヤー遷移有無を返す
     const uint8_t no = layer_ctrl_wait - 1;
     const layer_def_t*  const def = &layer_def[no];
     layer_ctrl_t* const ctrl = &layer_ctrl[no];
     
     if(def->retro) {
       // Hold優先時はここで初めてModを有効にする(→直後にMod対応でlキーが押される)
-      if(!def->ownlayer) {
-        layer_on(def->layer);
-        layer_cnt[def->layer]++;
-      }
-      else {
-        gHmodLayer = def->layer;
-      }
+      layer_on(def->layer);
+      layer_cnt[def->layer]++;
+      ret = true;
     }
     else {
       // Mod優先時は何もしない
@@ -429,7 +396,7 @@ static bool process_user_custom_layer_otherkey_down(void) {
     // 待ち状態終了    
     ctrl->state = LayerStateModHold;
     layer_ctrl_wait = 0;
-    return true;
+    return ret;
   }
   return false;
 }
@@ -463,141 +430,6 @@ static void process_user_custom_layer_time_check(void) {
   else {
     // 基準時刻を現在に更新する
     layer_ctrl_time = timer_read();
-  }
-}
-
-
-#define MOD_LCTL MOD_BIT(KC_LCTL)
-#define MOD_LSFT MOD_BIT(KC_LSFT)
-#define MOD_LALT MOD_BIT(KC_LALT)
-#define MOD_LWIN MOD_BIT(KC_LWIN)
-#define MOD_RCTL MOD_BIT(KC_RCTL)
-#define MOD_RSFT MOD_BIT(KC_RSFT)
-#define MOD_RALT MOD_BIT(KC_RALT)
-#define MOD_RWIN MOD_BIT(KC_RWIN)
-
-// hmodキーの出力を実際に行う関数
-static bool process_hmod_sendcode(uint8_t hmod_layer, uint8_t hmodno, bool pressed)
-{
-  switch(hmod_layer)
-  {
-  case 1:
-    if(pressed) {
-      switch(hmodno) {
-      case 1:
-        register_mods(MOD_LCTL);
-        break;
-      case 2:
-        register_mods(MOD_LSFT);
-        break;
-      case 3:
-        register_mods(MOD_LALT);
-        break;
-      case 4:
-        register_mods(MOD_LWIN);
-        break;
-      default:
-        return true;  // 処理値外はtrue
-      }
-    }
-    else {
-      switch(hmodno) {
-      case 1:
-        unregister_mods(MOD_LCTL);
-        break;
-      case 2:
-        unregister_mods(MOD_LSFT);
-        break;
-      case 3:
-        unregister_mods(MOD_LALT);
-        break;
-      case 4:
-        unregister_mods(MOD_LWIN);
-        break;
-      default:
-        return true;  // 処理値外はtrue
-      }
-    }
-    break;
-  case 2:
-    if(pressed) {
-      switch(hmodno) {
-      case 5:
-        register_mods(MOD_RCTL);
-        break;
-      case 6:
-        register_mods(MOD_RSFT);
-        break;
-      case 7:
-        register_mods(MOD_RALT);
-        break;
-      case 8:
-        register_mods(MOD_RWIN);
-        break;
-      default:
-        return true;  // 処理値外はtrue
-      }
-    }
-    else {
-      switch(hmodno) {
-      case 5:
-        unregister_mods(MOD_RCTL);
-        break;
-      case 6:
-        unregister_mods(MOD_RSFT);
-        break;
-      case 7:
-        unregister_mods(MOD_RALT);
-        break;
-      case 8:
-        unregister_mods(MOD_RWIN);
-        break;
-      default:
-        return true;  // 処理値外はtrue
-      }
-    }
-    break;
-  default:
-    return true;  // 処理値外はtrue
-  }
-  return false;
-}
-
-
-static bool process_hmod_event(uint8_t no, bool pressed)
-{
-  uint8_t hmodno = no - 16;
-  
-  // hmodキーでなければ通常キーとして処理
-  if((no < 16) && (no >= (16 + (sizeof(gHmodLastLayer)/sizeof(gHmodLastLayer[0]))))) {
-    return true;
-  }
-  
-  // hmod対象キー処理
-  if(pressed) {
-    // キーon時
-    if(gHmodLayer) {
-      // layerが有効な場合はレイヤー動作
-      gHmodLastLayer[hmodno] = gHmodLayer;  // on動作時のレイヤーを記録(off動作時に対応動作をさせるため)
-      return process_hmod_sendcode(gHmodLayer, hmodno, pressed);
-    }
-    else {
-      // 通常キーとして動作
-      return true;
-    }
-  }
-  else {
-    // キーoff動作
-    uint8_t hmodLastLayer = gHmodLastLayer[hmodno];
-    if(hmodLastLayer) {
-      // hmodのoff動作をさせて初期状態に戻す
-      gHmodLastLayer[hmodno] = 0;
-      return process_hmod_sendcode(hmodLastLayer, hmodno, pressed);
-    }
-    else {
-      // 通常キーとして動作させる
-      return true;
-    }
   }
 }
 
