3c0aacc094e34835662e1ca90694854f1e71e8e5
wwumiww <wumwum10000@gmail.com>
2022-06-12 19:01:24 +0900

    ergodashの設定を他とあわせて最新化

--- keyboards/ergodash/rev1/keymaps/mine/config.h
+++ keyboards/ergodash/rev1/keymaps/mine/config.h
@@ -31,3 +31,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MASTER_LEFT
 // #define MASTER_RIGHT
 // #define EE_HANDS
+
+// 自分用カスタマイズ
+#undef  TAPPING_TERM
+#undef  TAPPING_FORCE_HOLD
+#define TAPPING_TERM    200                 // タッピング判定時間
+
+#define TAPPING_TERM_PER_KEY                // key毎に tapping termを切り替える
+#define PERMISSIVE_HOLD_PER_KEY             // key毎に 許容ホールドを設定する
+#define HOLD_ON_OTHER_KEY_PRESS_PER_KEY     // key毎に hold機能優先を設定する
+#define IGNORE_MOD_TAP_INTERRUPT_PER_KEY    // key毎に モッドタップ割り込み無視を設定する
+#define TAPPING_FORCE_HOLD_PER_KEY          // key毎に タッピング強制holdを設定する
+
+// LCTL + ", RCTL + '
+#define LCPO_KEYS   KC_LCTL, KC_LSFT, KC_2  // [LCTL] + ["]
+#define RCPC_KEYS   KC_RCTL, KC_RSFT, KC_7  // [RCTL] + [']
