e19e37bb1b2ac3d4a8996831326e329706e6cc30
Ryan <fauxpark@gmail.com>
2024-08-04 21:28:40 +1000

    Small tweaks to keymap generation (#24240)

--- lib/python/qmk/tests/test_cli_commands.py
+++ lib/python/qmk/tests/test_cli_commands.py
@@ -163,7 +163,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #    include OTHER_KEYMAP_C
 #endif // OTHER_KEYMAP_C
 
-
 """
 
 
@@ -199,7 +198,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #ifdef OTHER_KEYMAP_C
 #    include OTHER_KEYMAP_C
 #endif // OTHER_KEYMAP_C
-
 
 """
 
