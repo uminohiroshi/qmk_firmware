aaa2fb7e7064a5b580470489835f7c85cf4916d7
UMINO Hiroshi <umino.hiroshi@gmail.com>
2024-09-22 13:58:35 +0900

    COMBO設定, CONSOLE出力など盛り込み

--- keyboards/dailycraft/claw44/keymaps/mine/config.h
+++ keyboards/dailycraft/claw44/keymaps/mine/config.h
@@ -23,10 +23,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 //#define DEBOUNCE                    10     // キーチャタリング対策
 
-#define LEADER_TIMEOUT              1000    // LEADER_PER_KEY_TIMEOUT使うときのmax=300
-#define LEADER_NO_TIMEOUT                   // 先頭のキーはタイムアウトなし
+//#define LEADER_TIMEOUT              1000    // LEADER_PER_KEY_TIMEOUT使うときのmax=300
+//#define LEADER_NO_TIMEOUT                   // 先頭のキーはタイムアウトなし
 
 #define DYNAMIC_MACRO_DELAY         20      // 再生delay
+#define DYNAMIC_KEYMAP_LAYER_COUNT  8
 
 
 // 自分用カスタマイズ
@@ -37,11 +38,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_TERM_PER_KEY                // key毎に tapping termを切り替える
 #define PERMISSIVE_HOLD_PER_KEY             // key毎に 許容ホールドを設定する
 #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY     // key毎に hold機能優先を設定する
-#define IGNORE_MOD_TAP_INTERRUPT_PER_KEY    // key毎に モッドタップ割り込み無視を設定する
-#define TAPPING_FORCE_HOLD_PER_KEY          // key毎に タッピング強制holdを設定する
+//#define TAPPING_FORCE_HOLD_PER_KEY          // key毎に タッピング強制holdを設定する
 
 // LCTL + ", RCTL + '
 //#define LCPO_KEYS   KC_LCTL, KC_LSFT, KC_2  // [LCTL] + ["] // 未使用
 #define RCPC_KEYS   KC_RCTL, KC_RSFT, KC_7  // [RCTL] + [']
 
+//#define CONSOLE_BAUD 921600
 
+// combo
+#define COMBO_TERM          10
+#define COMBO_STRICT_TIMER
+#define COMBO_TERM_PER_COMBO
